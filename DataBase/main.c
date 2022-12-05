2ade43c248577120f0954617e4b29080aeb70024
KostyaBIST221 <ksapov24@mail.ru>
2022-12-05 14:06:41 +0300

    Initial Commit

---
+++ DataBase/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DataBase
+//
+//  Created by kostya on 05.12.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
